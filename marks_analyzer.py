cdd2a9fb4c786f164f8019e59bc173ba4268856d
Krushnapriya Sahoo <krushnapriyas7@gmail.com>
2025-11-21 20:23:48 +0530

    Create marks_analyzer.py

---
+++ marks_analyzer.py
@@ -0,0 +1,9 @@
+marks = [78, 90, 67, 88, 92]
+
+avg = sum(marks) / len(marks)
+highest = max(marks)
+lowest = min(marks)
+
+print("Average:", avg)
+print("Highest:", highest)
+print("Lowest:", lowest)
